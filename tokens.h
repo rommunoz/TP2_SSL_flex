5b84d2d0fc47dcca27e1950db93a1da4df9a0f2d
rommunoz <rommunoz@frba.utn.edu.ar>
2023-10-27 20:47:12 -0300

    Primeros acercamientos, faltan todas las reglas y mis patrones hay muchos de mas. La negacion de mi alfabeto tmb es una duda (creo que mal hecho). No indenté nada por las dudas y no sé si va un %enn las definiciones

---
+++ tokens.h
@@ -0,0 +1,13 @@
+#ifndef tokens_h
+#define tokens_h
+#define e  2.7182818284
+#define pi 3.1415926536
+
+enum token {FDT = 0,
+            IDENTIFICADOR = 100 , NUMERO, CARACTER, OP_MENOS, OP_MAS, OP_MULTIP, OP_DIV, OP_EXP, 
+            ERR_COMUN = 200, NUM_INVALIDO, ID_INVALIDO};
+
+float nro = 00009.E-3;
+
+
+#endif
