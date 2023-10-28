f7a355554e2284476cfc2a0981930cbeeedac25e
rommunoz <rommunoz@frba.utn.edu.ar>
2023-10-27 23:02:17 -0300

    Notas y dudas, nada resuelto aun

--- tokens.h
+++ tokens.h
@@ -4,10 +4,9 @@
 #define pi 3.1415926536
 
 enum token {FDT = 0,
-            IDENTIFICADOR = 100 , NUMERO, CARACTER, OP_MENOS, OP_MAS, OP_MULTIP, OP_DIV, OP_EXP, 
+            IDENTIFICADOR = 100 , NUMERO, CARACTER, //OP_MENOS, OP_MAS, OP_MULTIP, OP_DIV, OP_EXP, 
             ERR_COMUN = 200, NUM_INVALIDO, ID_INVALIDO};
 
 float nro = 00009.E-3;
-
 
 #endif
