55e0355512a6f755624987d176f2f6c54a16a7b5
rommunoz <rommunoz@frba.utn.edu.ar>
2023-12-30 14:38:13 -0300

    Actualizar tokens.h

--- tokens.h
+++ tokens.h
@@ -3,9 +3,8 @@
 #define e  2.7182818284
 #define pi 3.1415926536
 
-//9 tokens
-enum token {FDT, PR_VAR, PR_SALIR, NL, IDENTIFICADOR, NUMERO, OP_MENOS_IG, OP_MAS_IG, OP_POR_IG, OP_DIV_IG}; 
-            /* PTESIS_ABRE = 40, PTESIS_CIERRA, OP_POR, OP_MAS, OP_MENOS = 45, OP_DIV = 47, 
-            OP_IG = 61, OP_EXP = 94};  */
+//10 tokens
+enum token {FDT, PR_VAR, PR_SALIR, NL, IDENTIFICADOR, NUMERO,
+             OP_MENOS_IG, OP_MAS_IG, OP_POR_IG, OP_DIV_IG}; 
 
 #endif 
